fa137045cc9d1e628f148d93b0857777be439778
LeapGenSolutions <website@leapgen.ai>
2025-06-13 13:56:32 -0400

    ci: add Azure Static Web Apps workflow file on-behalf-of: @Azure opensource@microsoft.com

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,8 @@
-export const BACKEND_URL = "https://seismic-backend-04272025-bjbxatgnadguabg9.centralus-01.azurewebsites.net/"
+export const BACKEND_URL = "https://seismic-app-server-test-gqd5bjfmgabwa9he.centralus-01.azurewebsites.net/"
 // export const BACKEND_URL = "http://localhost:8080"
-export const DOCTOR_PORTAL_URL = "https://calm-smoke-0a7368a0f.6.azurestaticapps.net/"
+export const DOCTOR_PORTAL_URL = "https://polite-bay-05120d00f.6.azurestaticapps.net/"
 export const SOS_URL = "https://seismicdockerbackend-test-e0ducsgtggh7ftat.centralus-01.azurewebsites.net/"
 export const STREAM_API_KEY = "qb4swzqqxr76"
-export const REDIRECT_URI = "http://localhost:3000/"
+// export const REDIRECT_URI = "http://localhost:3000/"
+export const REDIRECT_URI = "https://polite-ocean-02cddbb0f.6.azurestaticapps.net/"
 // export const 

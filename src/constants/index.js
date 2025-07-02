5ec9de37a08bdddee93883baf92bf0f19709cc62
LeapgenSolution <websites@leapgen.ai>
2025-07-01 21:44:01 -0500

    Changes to adapt links https://test.seismicconnect.com/

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,5 +4,5 @@ export const DOCTOR_PORTAL_URL = "https://polite-bay-05120d00f.6.azurestaticapps
 export const SOS_URL = "https://seismicdockerbackend-test-e0ducsgtggh7ftat.centralus-01.azurewebsites.net/"
 export const STREAM_API_KEY = "qb4swzqqxr76"
 // export const REDIRECT_URI = "http://localhost:3000/"
-export const REDIRECT_URI = "https://polite-ocean-02cddbb0f.6.azurestaticapps.net/"
+export const REDIRECT_URI = "https://test.seismicconnect.com/"
 export const CHATBOT_URL = "https://chat-strlit-gva3h7ekahgdctgk.centralus-01.azurewebsites.net";

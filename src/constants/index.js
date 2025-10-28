611feeebda1e9d664cbaff8a1956de631ef1e6a2
Anu Yammada <anuyammada@Anus-MacBook-Pro.local>
2025-10-28 12:14:21 -0400

    eleventh commit-index

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 export const BACKEND_URL = "https://seismic-app-server-test-gqd5bjfmgabwa9he.centralus-01.azurewebsites.net/"
 // export const BACKEND_URL = "http://localhost:8080"
-export const DOCTOR_PORTAL_URL = "https://patient-dev.seismicconnect.com/"
+export const DOCTOR_PORTAL_URL = "https://patient-test.seismicconnect.com/"
 export const SOS_URL = "https://seismicdockerbackend-test-e0ducsgtggh7ftat.centralus-01.azurewebsites.net/"
-export const STREAM_API_KEY = "72499ykcfb3z"
+export const STREAM_API_KEY = "qb4swzqqxr76"
 // export const REDIRECT_URI = "http://localhost:3000/"
-export const REDIRECT_URI = "https://dev.seismicconnect.com/"
+export const REDIRECT_URI = "https://test.seismicconnect.com/"
 export const CHATBOT_URL = "https://chat-strlit-gva3h7ekahgdctgk.centralus-01.azurewebsites.net";

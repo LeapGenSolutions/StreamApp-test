35899c11f2b6859c180670cbbe34edd19d22b0dc
Anu Yammada <anuyammada@Anus-MacBook-Pro.local>
2025-10-28 02:34:09 -0400

    ninth commit-backendurl

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://seismic-app-server-test-gqd5bjfmgabwa9he.centralus-01.azurewebsites.net/"
+export const BACKEND_URL = "https://seismic-backend-04272025-bjbxatgnadguabg9.centralus-01.azurewebsites.net/"
 // export const BACKEND_URL = "http://localhost:8080"
 export const DOCTOR_PORTAL_URL = "https://patient-test.seismicconnect.com/"
 export const SOS_URL = "https://seismicdockerbackend-test-e0ducsgtggh7ftat.centralus-01.azurewebsites.net/"

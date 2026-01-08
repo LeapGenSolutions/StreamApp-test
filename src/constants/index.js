373c34901bc24340732d9a7fb81fcfe05ff09ac2
LeapGenSolutions <website@leapgen.ai>
2026-01-08 09:35:57 -0500

    Update CHATBOT_URL to new test endpoint

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,4 +5,4 @@ export const SOS_URL = "https://seismicdockerbackend-test-e0ducsgtggh7ftat.centr
 export const STREAM_API_KEY = "qb4swzqqxr76"
 // export const REDIRECT_URI = "http://localhost:3000/"
 export const REDIRECT_URI = "https://test.seismicconnect.com/"
-export const CHATBOT_URL = "https://chat-strlit-gva3h7ekahgdctgk.centralus-01.azurewebsites.net";
+export const CHATBOT_URL = "https://sesmic-test.azurewebsites.net/";

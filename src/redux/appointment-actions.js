2ad8b439e09ed6acfce432e3e801394a01c47e1d
LeapGenSolutions <website@leapgen.ai>
2026-01-25 11:46:05 -0500

    Refactor fetchAppointmentDetails action
    Refactor fetchAppointmentDetails to include marking appointments as fetched and handle errors by dispatching empty appointments.

--- src/redux/appointment-actions.js
+++ src/redux/appointment-actions.js
@@ -1,36 +1,40 @@
 import { BACKEND_URL } from "../constants";
 import { appointmentActions } from "./appointment-slice";
 
+
 export const fetchAppointmentDetails = (email) => {
-    return async (dispatch) => {
-        const fetchAppointments = async () => {
-            const response = await fetch(
-                `${BACKEND_URL}api/appointments/${email}`
-            );
+ return async (dispatch) => {
+   const fetchAppointments = async () => {
+     const response = await fetch(
+       `${BACKEND_URL}api/appointments/${email}`
+     );
 
-            if (!response.ok) {
-                throw new Error('Could not fetch appointment data!');
-            }
 
-            const data = await response.json();
-            // const appts = data.map((appt) => {
-            //     const date = appt.id
-            //     const thatDaysAppts = appt.data.map((apptTemp) => {
-            //         return {
-            //             ...apptTemp,
-            //             date
-            //         }
-            //     })
-            //     return thatDaysAppts
-            // })
-            return data
-        }
+     if (!response.ok) {
+       throw new Error('Could not fetch appointment data!');
+     }
 
-        try {
-            const appointmentData = await fetchAppointments()
-            dispatch(appointmentActions.setAppointments(appointmentData))
-        } catch (error) {
-            throw new Error('Could not fetch appointment data!');
-        }
-    }
-}
+
+     const data = await response.json();
+     return data;
+   };
+
+
+   try {
+     const appointmentData = await fetchAppointments();
+
+
+     // existing dispatch (unchanged)
+     dispatch(appointmentActions.setAppointments(appointmentData));
+
+
+  
+     dispatch(appointmentActions.markAppointmentsFetched());
+
+
+   } catch (error) {
+     dispatch(appointmentActions.setAppointments([]));
+     dispatch(appointmentActions.markAppointmentsFetched());
+   }
+ };
+};

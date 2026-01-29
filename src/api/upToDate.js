b3f2d75e6e59b5f60a1183526aa9b81945c44a05
Anu Yammada <anuyammada@Anus-MacBook-Pro.local>
2026-01-29 08:37:47 -0500

    Revert "Add UptoDate api"
    This reverts commit 85ba289e2c533bf8a9159428d4f93f9a0eb28dc0.

--- src/api/upToDate.js
+++
@@ -1,9 +0,0 @@
-import { BACKEND_URL } from "../constants"; 
-
-export const fetchUpToDateRecommendation = async (apptId, userID) => {
-  const response = await fetch(`${BACKEND_URL}api/uptodate/recommendation/${apptId}/${userID}`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch clusters data");
-  }
-  return response.json();
-};
